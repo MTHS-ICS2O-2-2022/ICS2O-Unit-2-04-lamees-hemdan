125c656034372726104e57580a8839600e28d95c
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-03-22 17:29:23 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a triangle
+ */
+function calculate() {
+  // input
+  const Base = parseInt(document.getElementById('base').value)
+  const Height = parseInt(document.getElementById('height').value)
+
+  // process
+  const area = (Base * Height) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'The Area is ' + area + ' cm²'
+}
